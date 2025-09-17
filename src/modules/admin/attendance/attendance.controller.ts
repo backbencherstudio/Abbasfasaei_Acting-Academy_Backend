b37bddcc83794af63feab46a1d55d2aedb3b6fdf
Mozadded Ul Islam Noor <pmozadded@gmail.com>
2025-09-17 16:12:54 +0600

    attendance

---
+++ src/modules/admin/attendance/attendance.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { AttendanceService } from './attendance.service';
+import { CreateAttendanceDto } from './dto/create-attendance.dto';
+import { UpdateAttendanceDto } from './dto/update-attendance.dto';
+import { ApiResponse } from '@nestjs/swagger';
+
+
+@Controller('attendance')
+export class AttendanceController {
+  constructor(private readonly attendanceService: AttendanceService) {}
+
+  @ApiResponse({ description: 'Get All Attendance' })
+  @Get()
+  async getAllAttendance(
+    @Query() query: { status?: string; date?: string },
+  ) {
+    const { status, date } = query;
+    return this.attendanceService.getAllAttendance(status, date);
+  }
+}
